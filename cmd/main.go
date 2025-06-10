cd1d1c94ecbf6d6148a3792ceec2e6115604e94b
ArtemDaemon <artem.daemon.official@gmail.com>
2025-06-10 12:08:12 +0700

    Добавил авторизацию по токену

--- cmd/main.go
+++ cmd/main.go
@@ -3,14 +3,35 @@ package main
 import (
 	"fmt"
 	"go-file-soap/internal/api"
+	"go-file-soap/internal/middleware"
 	"log"
 	"net/http"
+	"os"
+
+	"github.com/go-chi/chi"
+	"github.com/joho/godotenv"
 )
 
 func main() {
-	http.HandleFunc("/soap", api.UploadJsonHandler)
-	http.HandleFunc("/soap.wsdl", api.WSDLHandler)
+	err := godotenv.Load()
+	if err != nil {
+		log.Fatal("Error while loading .env file:", err)
+	}
 
-	fmt.Println("SOAP service listening on http://localhost:8080/soap")
-	log.Fatal(http.ListenAndServe(":8080", nil))
+	expectedToken := os.Getenv("API_TOKEN")
+	if expectedToken == "" {
+		log.Fatal("API_TOKEN parameter not found in environment variables file:", err)
+	}
+
+	r := chi.NewRouter()
+
+	r.Route("/api", func(r chi.Router) {
+		r.Use(middleware.AuthMiddleware(expectedToken))
+		r.Post("/soap", api.UploadJsonHandler)
+	})
+
+	r.Get("/soap.wsdl", api.WSDLHandler)
+
+	fmt.Println("SOAP service listening on http://localhost:8080/api/soap")
+	log.Fatal(http.ListenAndServe(":8080", r))
 }
